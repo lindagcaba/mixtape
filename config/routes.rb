78951b335f1791f6bc23104783ef82b5ad24a7ae
lindagcaba <lindagcaba@gmail.com>
2011-11-06 18:09:59 -0500

    created static pages -home,about,contact,help

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Mixtape::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
+  get 'pages/help'
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe PagesController do
+  render_views
+  
+  before(:each) do
+   @app_name = "Mixtape"
+  end
+ 
+ 
+  describe "GET 'home'"do
+    it"should be successful" do
+      get 'home'
+      response.should be_success
+    end 
+    it"should have the right title"do 
+      get'home'
+      response.should have_selector("title", :content => @app_name + "|Home")
+    end
+  end
+ 
+  describe "GET 'about'"do
+    it"should be successful" do
+      get 'about'
+      response.should be_success	   
+   end
+   it"should have the right title" do 
+     get 'about'
+     response.should have_selector("title", :content => @app_name + "|About") 
+   end
+  end
+ 
+  describe "GET 'contact'"do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => @app_name + "|Contact")
+    end
+  end 
+ 
+  describe "GET 'help'" do
+    it"should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title"do
+      get 'help'
+      response.should have_selector("title", :content => @app_name + "|Help")
+    end 
+  end
+end
