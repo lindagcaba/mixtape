d09790cac61397810627d0b3eea11f4f067e3af2
lindagcaba <lindagcaba@gmail.com>
2011-11-08 03:48:56 -0500

    now have some layout with design using bootstrap api,but only static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Mixtape::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  get 'pages/help'
+  
+ 
+  match '/about', :to => 'pages#about'
+  match '/contact', :to => 'pages#contact'
+  match '/help', :to => 'pages#help'
+ 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -51,7 +53,7 @@ Mixtape::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
