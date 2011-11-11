7ab547b9808e8bd18eaa913ea080b2d2d84adc41
lindagcaba <lindagcaba@gmail.com>
2011-11-11 18:51:44 -0500

    database cleanup after some schema mistakes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111108085328) do
+ActiveRecord::Schema.define(:version => 20111111231250) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -18,5 +18,7 @@ ActiveRecord::Schema.define(:version => 20111108085328) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
